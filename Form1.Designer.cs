41e55f7842c44d22ad6911f1855d175aab76118b
mariolberto <mariocabrerac8@gmail.com>
2023-06-30 22:21:53 -0500

    TERCER AVANCE

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -159,7 +159,7 @@ namespace ProyectofinalSAT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(587, 490);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
